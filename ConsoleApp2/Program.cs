e71a90419c5c20dcbf151859397e10a9957c45fa
Justice Engle <justice.engle@icloud.com>
2023-12-10 20:55:53 -0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using SSClient = Microsoft.Data.SqlClient;
+
+namespace Flashcards;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+        using (var connection = new SSClient.SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            Console.WriteLine("Connection opened successfully...\n\nPress any key to finish...");
+
+            Console.ReadKey();
+        }
+    }
+}
