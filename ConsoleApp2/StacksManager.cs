0551a6f180edc2dec50d12cc02f5da950c5dabff
Justice Engle <justice.engle@icloud.com>
2023-12-12 02:10:19 -0500

    added comments to make code easier to follow

--- ConsoleApp2/StacksManager.cs
+++ ConsoleApp2/StacksManager.cs
@@ -7,11 +7,14 @@ namespace Flashcards;
 
 internal class StacksManager
 {
+    // holds all Stacks to display
     private static List<Stack> stacks = new List<Stack>();
 
+    // connection string for Flashcards DB
     private static string connectionString = ConfigurationManager.AppSettings.Get("FlashcardsConnectionString");
     internal static void ManageStacks()
     {
+        // calls method that displays Stacks to user
         StacksManager.PrintStacks();
     }
 
@@ -26,10 +29,12 @@ internal class StacksManager
                 command.CommandType = System.Data.CommandType.Text;
                 command.CommandText = @"SELECT name FROM Stacks ORDER BY id";
 
+                // executes query that returns a SQL Data Reader obj
                 var reader = command.ExecuteReader();
 
                 if (reader.HasRows)
                 {
+                    // while reader obj has rows to read, add that Stack to the Stacks list, but only the name
                     while (reader.Read())
                     {
                         stacks.Add(new Stack
@@ -43,6 +48,7 @@ internal class StacksManager
                     Console.WriteLine("\n\nNo rows found.");
                 }
 
+                // uses ConsoleTableExt NuGet package library to display List of Stacks as a table
                 ConsoleTableBuilder.From(stacks).ExportAndWriteLine();
                 Console.WriteLine("\n\n");
 
