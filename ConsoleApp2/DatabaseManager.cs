307be3811dbaaf4f23e89231cdd1c6d204efe7f8
Justice Engle <justice.engle@icloud.com>
2024-01-18 00:03:02 -0500

    deleted unused code.

--- ConsoleApp2/DatabaseManager.cs
+++ ConsoleApp2/DatabaseManager.cs
@@ -425,19 +425,3 @@ PIVOT
         }
     }
 }
-
-internal enum Months
-{
-    January = 1,
-    February,
-    March,
-    April,
-    May,
-    June,
-    July,
-    August,
-    September,
-    October,
-    November,
-    December
-}
